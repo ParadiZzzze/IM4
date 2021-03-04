0f8ea8929ea2304e53103fdaf093b3b87a8dcf3b
ParadiZzzze <73051260+ParadiZzzze@users.noreply.github.com>
2021-03-04 16:32:46 +0700

    Add files via upload

---
+++ Currency/Cur/Form1.Designer.cs
@@ -0,0 +1,229 @@
+﻿namespace Cur
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.labBTC = new System.Windows.Forms.Label();
+            this.labDollars = new System.Windows.Forms.Label();
+            this.edAmount = new System.Windows.Forms.NumericUpDown();
+            this.buttonSell = new System.Windows.Forms.Button();
+            this.buttonBuy = new System.Windows.Forms.Button();
+            this.btStart = new System.Windows.Forms.Button();
+            this.edDays = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.edRate = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.edAmount)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.edDays)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.edRate)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.labBTC);
+            this.panel1.Controls.Add(this.labDollars);
+            this.panel1.Controls.Add(this.edAmount);
+            this.panel1.Controls.Add(this.buttonSell);
+            this.panel1.Controls.Add(this.buttonBuy);
+            this.panel1.Controls.Add(this.btStart);
+            this.panel1.Controls.Add(this.edDays);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.edRate);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(717, 82);
+            this.panel1.TabIndex = 0;
+            // 
+            // labBTC
+            // 
+            this.labBTC.AutoSize = true;
+            this.labBTC.Location = new System.Drawing.Point(302, 51);
+            this.labBTC.Name = "labBTC";
+            this.labBTC.Size = new System.Drawing.Size(40, 13);
+            this.labBTC.TabIndex = 9;
+            this.labBTC.Text = "BTC: 0";
+            // 
+            // labDollars
+            // 
+            this.labDollars.AutoSize = true;
+            this.labDollars.Location = new System.Drawing.Point(302, 37);
+            this.labDollars.Name = "labDollars";
+            this.labDollars.Size = new System.Drawing.Size(60, 13);
+            this.labDollars.TabIndex = 8;
+            this.labDollars.Text = "USD: 1000";
+            // 
+            // edAmount
+            // 
+            this.edAmount.DecimalPlaces = 2;
+            this.edAmount.Location = new System.Drawing.Point(473, 14);
+            this.edAmount.Name = "edAmount";
+            this.edAmount.Size = new System.Drawing.Size(96, 20);
+            this.edAmount.TabIndex = 7;
+            // 
+            // buttonSell
+            // 
+            this.buttonSell.Location = new System.Drawing.Point(386, 11);
+            this.buttonSell.Name = "buttonSell";
+            this.buttonSell.Size = new System.Drawing.Size(75, 23);
+            this.buttonSell.TabIndex = 6;
+            this.buttonSell.Text = "Sell";
+            this.buttonSell.UseVisualStyleBackColor = true;
+            this.buttonSell.Click += new System.EventHandler(this.buttonSell_Click);
+            // 
+            // buttonBuy
+            // 
+            this.buttonBuy.Location = new System.Drawing.Point(304, 11);
+            this.buttonBuy.Name = "buttonBuy";
+            this.buttonBuy.Size = new System.Drawing.Size(75, 23);
+            this.buttonBuy.TabIndex = 5;
+            this.buttonBuy.Text = "Buy";
+            this.buttonBuy.UseVisualStyleBackColor = true;
+            this.buttonBuy.Click += new System.EventHandler(this.buttonBuy_Click);
+            // 
+            // btStart
+            // 
+            this.btStart.Location = new System.Drawing.Point(208, 27);
+            this.btStart.Name = "btStart";
+            this.btStart.Size = new System.Drawing.Size(75, 23);
+            this.btStart.TabIndex = 4;
+            this.btStart.Text = "Simmulate";
+            this.btStart.UseVisualStyleBackColor = true;
+            this.btStart.Click += new System.EventHandler(this.btStart_Click);
+            // 
+            // edDays
+            // 
+            this.edDays.Location = new System.Drawing.Point(106, 45);
+            this.edDays.Name = "edDays";
+            this.edDays.Size = new System.Drawing.Size(96, 20);
+            this.edDays.TabIndex = 3;
+            this.edDays.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Дней";
+            // 
+            // edRate
+            // 
+            this.edRate.DecimalPlaces = 2;
+            this.edRate.Location = new System.Drawing.Point(106, 16);
+            this.edRate.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.edRate.Name = "edRate";
+            this.edRate.Size = new System.Drawing.Size(96, 20);
+            this.edRate.TabIndex = 1;
+            this.edRate.Value = new decimal(new int[] {
+            7433,
+            0,
+            0,
+            131072});
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Текущий курс";
+            // 
+            // chart1
+            // 
+            chartArea2.AxisX.Minimum = 0D;
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
+            this.chart1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.chart1.Location = new System.Drawing.Point(0, 82);
+            this.chart1.Name = "chart1";
+            series2.BorderWidth = 3;
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series2.IsValueShownAsLabel = true;
+            series2.LabelFormat = "F2";
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(717, 368);
+            this.chart1.TabIndex = 1;
+            this.chart1.Text = "chart1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(717, 450);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.edAmount)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.edDays)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.edRate)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btStart;
+        private System.Windows.Forms.NumericUpDown edDays;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown edRate;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Label labBTC;
+        private System.Windows.Forms.Label labDollars;
+        private System.Windows.Forms.NumericUpDown edAmount;
+        private System.Windows.Forms.Button buttonSell;
+        private System.Windows.Forms.Button buttonBuy;
+    }
+}
+
---
+++ Currency/Cur/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cur
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        const double k = 0.05;
+        public double USD = 1000;
+        public double BTC = 0;
+        private void btStart_Click(object sender, EventArgs e)
+        {
+            Random random = new Random();
+            double rate = (double)edRate.Value;
+            int days = (int)edDays.Value;
+            chart1.Series[0].Points.Clear();
+            chart1.Series[0].Points.AddXY(0, rate);
+
+            for (int i = 1; i <= days; i++)
+            {
+                rate = rate * (1 + k * (random.NextDouble() - 0.5));
+                edRate.Value = (decimal)rate;
+                chart1.Series[0].Points.AddXY(i, rate);
+            }
+        }
+
+        private void buttonBuy_Click(object sender, EventArgs e)
+        {
+            double amount = (double)edAmount.Value;
+            double rate = (double)edRate.Value;
+            double jojo = rate * amount;
+            if (jojo<=USD)
+            {
+                USD -= jojo;
+                BTC += amount;
+            }
+            labBTC.Text = "BTC: " + BTC;
+            labDollars.Text = "USD: " + USD;
+        }
+
+        private void buttonSell_Click(object sender, EventArgs e)
+        {
+            double amount = (double)edAmount.Value;
+            double rate = (double)edRate.Value;
+            double jojo = amount;
+            if (jojo <= BTC)
+            {
+                BTC -= amount;
+                USD += amount*rate;
+            }
+            labBTC.Text = "BTC: " + BTC;
+            labDollars.Text = "USD: " + USD;
+        }
+    }
+}
